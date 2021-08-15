957f919de8e546fd5a8e08fbac5b008278bd81be
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-08-14 22:50:00 -0500

    init
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//Fetch the csv file and return it up
+const fetchCSV = async (path) => {
+  const response = await fetch(path);
+  const table = await response.text();
+  return table;
+}; 
+//Transform the csv data to an array of js objects
+const csvParser = async (data) => {
+  //1. split de data by break lines and commas in a single movement
+  //-> Expected result: [val1, val2, val3... ]
+  const transformed = data.split(/\r?\n/).map(data => data.split(','));
+  //Get the headers
+  const headers = transformed[0];
+  //rid of the headers
+  transformed.splice(0, 1);
+  //Design the array elem according to the headers
+  const obj_data = transformed.map(data => {
+    const current = {};
+    headers.forEach((i, index) => current[i] = data[index])
+    return current;
+  })
+  return obj_data;
+};
+
+//USE
+const dataAsString = fetchCSV('../data/sp.csv');
+const converted = dataAsString.then(value => {
+  csvParser(value).then(result => console.table(result)); 
+});
