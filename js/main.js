bd39b144976d774727fe3aeba4620e14843bfdf6
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-08-16 12:52:15 -0500

    selectors in other file and impor it inside main.js
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,26 +2,18 @@
 import { sortingHandler } from './modules/sort.js';
 import { fetchCSV } from './modules/fetchCSV.js';
 import { csvParser } from './modules/csvParser.js';
-//Selectors
-const form = document.querySelector('.form');
-const submit = form.submit; 
-const column = form.column;
-const order = form.order; 
+import { submit, column, order } from './modules/selectors.js';
 //USE
-const dataAsString = fetchCSV('../data/sp.csv');
-const converted = dataAsString.then(value => {
-  return csvParser(value);  
-});
+const dataConverted = fetchCSV('../data/sp.csv').then(csvParser);
 //Events
 submit.addEventListener('click', (event) => {
   event.preventDefault();
-  // console.clear();
-  console.log(order.value);
+  console.clear();
   const ascendent = order.value === '1' ? true : false; 
-  converted.then(result => {
+  dataConverted.then(result => {
     const test = sortingHandler({
       data: result, 
-      ascendent: ascendent , 
+      ascendent: ascendent, 
       column: column.value
     })
     console.table(test)
---
+++ js/modules/selectors.js
@@ -0,0 +1,5 @@
+//Selectors
+export const form = document.querySelector('.form');
+export const submit = form.submit; 
+export const column = form.column;
+export const order = form.order; 
